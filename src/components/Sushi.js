a86cec204c4dcf4472d1fefb609a55a254887dbc
Brandi Burnell <brandiburnell6@gmail.com>
2023-12-16 19:47:30 -0700

    pass half tests

--- src/components/Sushi.js
+++ src/components/Sushi.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-function Sushi(props) {
+function Sushi({image, id, name, price, createdDate}) {
   return (
     <div className="sushi">
-      <div className="plate" onClick={/* Give me a callback! */ null}>
+      <div className="plate" onClick={(e) => console.log(id)}>
         {/* Tell me if this sushi has been eaten! */}
         {false ? null : (
           <img
-            src={/* Give me an image source! */ null}
-            alt={/* Give me a name! */ "Sushi"}
+            src={image}
+            alt={name}
             width="100%"
           />
         )}
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {name} - ${price}
       </h4>
     </div>
   );
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
 function SushiContainer(props) {
+  const [sushis, setSushis] = useState([]);
+  const [endIndex, setEndIndex] = useState(4);
+  const [startIndex, setStartIndex] = useState(0);
+
+  const selectedSushis = [...sushis].slice(startIndex, endIndex);
+
+  console.log(selectedSushis);
+
+  // fetch sushis to display on inital render
+  useEffect(() => {
+    fetch("http://localhost:3001/sushis")
+      .then((r) => r.json())
+      .then(sushis => setSushis(sushis));
+  },[]);
+
+  // change indexes when More Sushi button is clicked
+  function handleMoreSushi() {
+    setStartIndex(startIndex + 4);
+    setEndIndex(endIndex + 4);
+  }
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {selectedSushis.map((sushi) => {
+          return ( <Sushi 
+                    image={sushi.img_url}
+                    key={sushi.id}
+                    name={sushi.name}
+                    price={sushi.price}
+                    id={sushi.id}/>
+          );
+        })
+      }
+      <MoreButton onMoreClick={handleMoreSushi}/>
     </div>
   );
 }
